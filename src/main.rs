ef532f7af05b8d9067a30e8f90a695eb4335a5f0
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-30 23:13:17 -0400

    practice displaying custom data based on stored enum data

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-fn main() {
-    //let today = Weekdays::Monday(Class{ course: String::from("Rust"), teacher: String::from("Herbert Wolverson"), grade: 4.5});
-    let tomorrow = Weekdays::Thursday(Work{ hours: 5.00, pay: 75.00, shift: Shift::Night});
+use std::fmt;
 
-    match tomorrow {
-        Weekdays::Monday(class) => println!("time for school"),
-        Weekdays::Wednesday(class) => println!("time for school"),
-        Weekdays::Friday(class) => println!("time for school"),
-        _ => println!("time for work")
+fn main() {
+    let today = Weekdays::Monday(Class{ course: String::from("Rust"), teacher: String::from("Herbert Wolverson"), grade: 4.5});
+    //let tomorrow = Weekdays::Thursday(Work{ hours: 5.00, pay: 75.00, shift: Shift::Night});
+
+    match today {
+        Weekdays::Monday(class) => print_class_info(&class),
+        Weekdays::Tuesday(work) => print_work_info(&work),
+        Weekdays::Wednesday(class) => print_class_info(&class),
+        Weekdays::Thursday(work) => print_work_info(&work),
+        Weekdays::Friday(class) => print_class_info(&class),
     }
+}
+
+fn print_class_info(class_info: &Class) {
+    println!("course: {} taught by {}", class_info.course, class_info.teacher);
+}
+
+fn print_work_info(work_info: &Work) {
+    println!("earned ${} over {} hours during the {} shift", work_info.pay, work_info.hours, work_info.shift.to_string());
 }
 
 enum Weekdays {
@@ -28,6 +39,17 @@ enum Shift {
     Early,
     Afternoon,
     Night
+}
+
+impl fmt::Display for Shift {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Shift::Early => write!(f, "early"),
+            Shift::Afternoon => write!(f, "afternoon"),
+            Shift::Night => write!(f, "night"),
+        }
+
+    }
 }
 
 struct Work {
