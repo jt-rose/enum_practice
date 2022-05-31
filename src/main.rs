6db9191b61c63a823fb03e5ac4793ce4f9326890
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-30 22:49:30 -0400

    practice using stored data in enums

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 fn main() {
-    let today = Weekdays::Monday;
+    //let today = Weekdays::Monday(Class{ course: String::from("Rust"), teacher: String::from("Herbert Wolverson"), grade: 4.5});
+    let tomorrow = Weekdays::Thursday(Work{ hours: 5.00, pay: 75.00, shift: Shift::Night});
 
-    match today {
-        Weekdays::Monday => println!("Here we go!"),
-        Weekdays::Wednesday => println!("Hump day"),
-        Weekdays::Friday => println!("Yay!"),
-        _ => println!("just another day")
+    match tomorrow {
+        Weekdays::Monday(class) => println!("time for school"),
+        Weekdays::Wednesday(class) => println!("time for school"),
+        Weekdays::Friday(class) => println!("time for school"),
+        _ => println!("time for work")
     }
 }
 
 enum Weekdays {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday
+    Monday(Class),
+    Tuesday(Work),
+    Wednesday(Class),
+    Thursday(Work),
+    Friday(Class)
+}
+
+struct Class {
+    course: String,
+    teacher: String,
+    grade: f64
+}
+
+enum Shift {
+    Early,
+    Afternoon,
+    Night
+}
+
+struct Work {
+    hours: f64,
+    pay: f64,
+    shift: Shift
 }
 
