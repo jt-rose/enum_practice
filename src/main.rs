0fd4126f3cf763e69cef2021b8621cc396688798
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-30 22:38:09 -0400

    add enum and match practice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let today = Weekdays::Monday;
+
+    match today {
+        Weekdays::Monday => println!("Here we go!"),
+        Weekdays::Wednesday => println!("Hump day"),
+        Weekdays::Friday => println!("Yay!"),
+        _ => println!("just another day")
+    }
 }
+
+enum Weekdays {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday
+}
+
